3993a6ae4c93304a711a4bcb4cef223655f10d19
Shreya Tiwari <90816107+shreyatiwari12@users.noreply.github.com>
2022-08-10 16:07:01 +0530

    Add files via upload

---
+++ Springbootmongodbbackend/src/main/java/com/example/krishna/KrishnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.krishna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishnaApplication.class, args);
+	}
+
+}
